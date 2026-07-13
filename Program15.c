3c84448c0353a873e08c8c6411e91df22a9081c6
Arun Nagappa Naik <narun9315@gmail.com>
2026-07-13 20:19:41 +0530

    Add files via upload

---
+++ Program15.c
@@ -0,0 +1,17 @@
+#include<reg51.h>
+sbit LED=P1^0;
+void delay(unsigned int t){
+	unsigned int i;
+	while(t--){
+		for(i=0; i<1275; i++);
+	}
+}
+void main(void){
+	while(1){
+	LED=0;
+	delay(10);
+	LED=1;
+	delay(20);
+	}
+}
+	
