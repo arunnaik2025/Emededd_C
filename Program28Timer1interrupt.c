f4566e6dc796deed16aa1c290b4dd2ff90b343a4
Arun Nagappa Naik <narun9315@gmail.com>
2026-07-18 11:01:28 +0530

    Add files via upload

---
+++ Program28Timer1interrupt.c
@@ -0,0 +1,19 @@
+#include<reg51.h>
+sbit LED=P1^0;
+void Timer1_ISR(void)interrupt 3{
+	TH1=0xFC;
+	TL1=0x66;
+	LED=~LED;
+}
+void main(void){
+	LED=1;
+	TMOD=0x10;
+	TH1=0xFC;
+	TL1=0x66;
+	EA=1;
+	ET1=1;
+	TR1=1;
+	while(1){
+	}
+}
+	
