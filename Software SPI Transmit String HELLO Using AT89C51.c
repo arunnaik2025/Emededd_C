c97b634139672021c44de4674021c82692c36d79
Arun Nagappa Naik <narun9315@gmail.com>
2026-07-22 17:11:25 +0530

    Add files via upload

---
+++ Software SPI Transmit String HELLO Using AT89C51.c
@@ -0,0 +1,45 @@
+#include<reg51.h>
+sbit MOSI=P1^0;
+sbit SS=P1^1;
+sbit SCK=P1^2;
+sbit MISO=P1^3;
+void SPI_init(void){
+	SS=1;
+	SCK=0;
+	MOSI=0;
+}
+void SPI_Delay(void){
+	unsigned int i;
+	unsigned int j;
+	for(i=0;i<500;i++){
+		for(j=0;j<1275;j++);
+	}
+}
+void SPI_Tx(unsigned char txdata){
+	unsigned char i=0;
+	while(i<8){
+		if(txdata&0x80){
+			MOSI=1;
+		}
+		else{
+			MOSI=0;
+		}
+		SPI_Delay();
+		SCK=1;
+		SPI_Delay();
+		SCK=0;
+		txdata=txdata<<1;
+		i++;
+	}
+}
+void main(void){
+	unsigned char str[]="HELLO";
+	unsigned char i=0;
+	SPI_init();
+	while(1){
+		for(i=0; str[i]!='\0'; i++){
+			SPI_Tx(str[i]);
+	}
+		SPI_Delay();
+}
+}
