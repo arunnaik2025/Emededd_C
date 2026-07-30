74039dd32fda8e196588638848d6817012e83fa3
Arun Nagappa Naik <narun9315@gmail.com>
2026-07-30 22:12:16 +0530

    Add files via upload

---
+++ Software I²C EEPROM Read and Write using AT89C51.c
@@ -0,0 +1,117 @@
+#include<reg51.h>
+sbit SDA=P1^0;
+sbit SCL=P1^1;
+void init(void){
+	SDA=1;
+	SCL=1;
+}
+void delay(void){
+	unsigned int i;
+	unsigned int j;
+	for(i=0; i<500; i++){
+		for(j=0; j<1275; j++);
+	}
+}
+void start(void){
+	SDA=1;
+	SCL=1;
+	delay();
+	SDA=0;
+	SCL=0;
+	delay();
+}
+void write(unsigned char tdata){
+	unsigned char i;
+	for(i=0; i<8; i++){
+		if(tdata&0x80){
+			SDA=1;
+		}
+		else{
+			SDA=0;
+		}
+		SCL=1;
+		delay();
+		SCL=0;
+		delay();
+		tdata<<=1;
+	}
+}
+bit ACK(void){
+	SDA=1;
+	SCL=1;
+	delay();
+	if(SDA==0){
+		SCL=0;
+		return 1;
+	}
+	SCL=0;
+	return 0;
+}
+unsigned char read(void){
+	unsigned char rdata=0;
+	unsigned char i=0;
+	SDA=1;
+	for(i=0; i<8; i++){
+		SCL=1;
+		delay();
+		rdata<<=1;
+		if(SDA){
+			rdata|=0x01;
+		}
+		SCL=0;
+		delay();
+	}
+	return rdata;
+}
+void NACK(void){
+	SDA=1;
+	SCL=1;
+	delay();
+	SCL=0;
+	delay();
+}
+void stop(void){
+	SDA=0;
+	SCL=1;
+	delay();
+	SDA=1;
+}
+void checkACK(void){
+	if(ACK()==0){
+		SCL=0;
+		while(1);
+	}
+}
+void main(void){
+	unsigned char rdata;
+	init();
+	while(1){
+		start();
+		write(0xA0);
+		checkACK();
+		
+		write(0x20);
+		checkACK();
+		
+		write(0x55);
+		checkACK();
+		stop();
+		delay();
+		
+		start();
+		write(0xA0);
+		checkACK();
+		
+		write(0x20);
+		checkACK();
+		
+		start();
+		write(0xA1);
+		checkACK();
+		
+		rdata=read();
+		NACK();
+		stop();
+		}
+	}
+			
