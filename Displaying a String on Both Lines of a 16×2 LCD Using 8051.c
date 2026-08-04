c73e0c75f812c93b34e11acad4dc5db98a9b7713
Arun Nagappa Naik <narun9315@gmail.com>
2026-08-04 12:31:25 +0530

    Add files via upload

---
+++ Displaying a String on Both Lines of a 16×2 LCD Using 8051.c
@@ -0,0 +1,53 @@
+#include<reg51.h>
+sbit RW=P1^0;
+sbit RS=P1^1;
+sbit EN=P1^2;
+void delay(void);
+void LCD_command(unsigned char cmd);
+void LCD_data(unsigned char ch);
+void LCD_string(char *str);
+void LCD_init(void);
+void delay(void){
+	unsigned int i;
+	unsigned int j;
+	for(i=0; i<500; i++){
+		for(j=0; j<1275; j++){
+		}
+	}
+}
+void LCD_command(unsigned char cmd){
+	RS=0;
+	RW=0;
+	P2=cmd;
+	EN=1;
+	delay();
+	EN=0;
+}
+void LCD_data(unsigned char ch){
+	RS=1;
+	RW=0;
+	P2=ch;
+	EN=1;
+	delay();
+	EN=0;
+}
+void LCD_string(char *str){
+	while(*str!='\0'){
+		LCD_data(*str);
+		str++;
+	}
+}
+void init(void){
+	LCD_command(0x38);
+	LCD_command(0x0C);
+	LCD_command(0x06);
+	LCD_command(0x01);
+}
+void main(void){
+	init();
+	LCD_command(0x80);
+	LCD_string("Arun");
+	LCD_command(0xC0);
+	LCD_string("Naik");
+	while(1);
+}
